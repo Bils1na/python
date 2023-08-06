a61bc0a6202de29ed5217ff766998a10f1d29c82
bilsina <bils1n@icloud.com>
2023-08-06 12:52:10 +0300

    add lecture1

---
+++ main.py
@@ -0,0 +1,70 @@
+# a = "Hello" 
+# b = 7
+# c = 7.8
+
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a, b, c))
+
+# d = input("Enter first string >> ")
+# print(d)
+
+
+# e = int(input())
+# f = int(input())
+# print(e, " + ", f, " = ", e + f)
+
+# v = "fdafd"
+# v =int(v)
+
+
+# af = 5.3354
+# bf = 4.64570
+# print(round(af * bf, 3))
+
+# iter = 2
+# iter += 3
+# iter -= 4
+
+# username = input("Enter name: ")
+# if username == "Artem":
+#     print("Hooray, this is Artem!")
+# else:
+#     print("Hello!")
+
+# i = 0 
+# while i < 5:
+#     # if i == 3:
+#     #     break
+#     i = i + 1
+# else: 
+#     print("Well")
+#     print("It's enough")
+# print(i)
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print(i)
+#     elif i > n // 2:
+#         print(n)
+#         flag = False
+#     i += 1
+
+# a = "qwerty"
+
+# print(a[0])
+# print(a[2])
+
+# for i in range(5):
+#     print(a[i])
+
+text = "Hello, World!"
+
+print(len(text))
+print(text.lower())
+print(text.upper())
+print(text.replace('Hello', 'hELLO'))
+print(text[::-1])
