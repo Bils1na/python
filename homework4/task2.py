47e94854a3ea8ebf91282a838f3269195db1b1d0
bilsina <bils1n@icloud.com>
2023-09-04 19:52:18 +0300

    added hw4

---
+++ homework4/task2.py
@@ -0,0 +1,29 @@
+# задача 2 необязательная. Напишите программу, 
+# которая получает целое число и возвращает его двоичное, 
+# восьмеричное строковое представление.
+# Функции bin и oct используйте для проверки своего результата.
+# *Дополнительно
+# Попробуйте избежать дублирования кода в преобразованиях к 
+# разным системам счисления
+# Избегайте магических чисел
+# Добавьте аннотацию типов где это возможно
+# Используйте функции
+
+def invert_number(dec_number, format):
+    result = ""
+
+    while dec_number >= 1:
+        result += str(dec_number % format)
+        dec_number //= format
+    
+    return result
+
+
+OCT = 8
+BIN = 2
+
+number = int(input("Enter number >> "))
+bin_number = invert_number(number, BIN)
+oct_number = invert_number(number, OCT)
+
+print(f"dec = {number}, bin = {bin_number[::-1]}, oct = {oct_number[::-1]}")
