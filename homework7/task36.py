82f5e53c61a3cfe0eceb0ea8d3b2afb155b856bb
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-09-11 13:18:55 +0300

    fix task36

--- homework7/task36.py
+++ homework7/task36.py
@@ -4,13 +4,10 @@
 # быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). 
 # Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, 
 # как, например, у операции умножения.
-
 # # *Пример:*
-
 # # **Ввод:** `print_operation_table(lambda x, y: x * y) ` 
 # # **Вывод:**
 # # 1 2 3 4 5 6
-
 # # 2 4 6 8 10 12
 # # 3 6 9 12 15 18
 # # 4 8 12 16 20 24
@@ -18,10 +15,10 @@
 # # 6 12 18 24 30 36
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    result = list(map(lambda x: list(map(lambda y: operation(x, y), range(1, num_columns+1))), range(1, num_rows+1)))
-    for lst in result:
-        for i in lst:
-            print(i, end=" ")
-        print("")
+    result = list(map(lambda x: list(map(lambda y: operation(x, y), 
+            range(1, num_columns+1))), range(1, num_rows+1)))
+    for i in range(num_rows):
+        print(*result[i])
+
 
 print_operation_table(lambda x, y: x * y)
