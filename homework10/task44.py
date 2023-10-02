9ffd5f73ef827f5e5e17f1ef069aa801d1bc0a3b
bilsina <bils1n@icloud.com>
2023-10-02 19:58:59 +0300

    added hw10

---
+++ homework10/task44.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+print(data)
+print()
+
+one_hot = pd.DataFrame()
+
+for value in data["whoAmI"].unique():
+    one_hot[value] = (data["whoAmI"] == value).astype(bool)
+
+print(one_hot)
